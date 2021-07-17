3d233de4b259593a66040370acaa0ada0ad318a0
Kyaw Thu <80441413+evanch98@users.noreply.github.com>
2021-07-18 02:12:03 +0800

    Update multiplicationQuiz.py

--- multiplicationQuiz.py
+++ multiplicationQuiz.py
@@ -4,11 +4,14 @@ import random, time
 numberofQuestions = 10
 correctAnswers = 0
 for questionNumber in range(numberofQuestions):
+    #Pick two random numbers
     num1 = random.randint(0, 9)
     num2 = random.randint(0, 9)
 
     prompt = '#%s: %s x %s' % (questionNumber, num1, num2)
     try:
+        #Right answers are handled by allowRegexes
+        #Wrong answer are handled by blockRegexes, with a custom message
         pyip.inputStr(prompt, allowRegexes=['^%s$' % (num1 * num2)],
                               blockRegexes=[('.*', 'Incorrect!')],
                               timeout=8, limit=3)
@@ -17,8 +20,9 @@ for questionNumber in range(numberofQuestions):
     except pyip.RetryLimitException:
         print('Out of tries!')
     else:
+        #This block runs if no exception were raised in the try block
         print('Correct!')
         correctAnswers += 1
-        time.sleep(1)
+        time.sleep(1)   #Brief pause to let user see the result
 
 print('Score: %s / %s' % (correctAnswers, numberofQuestions))
